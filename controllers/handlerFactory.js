5835d655fa8d48f68ceaf79bc1da67e34affa07e
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-07-01 18:10:33 +0500

    add getOne, getAll

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -28,6 +29,61 @@ exports.updateOne = (Model) =>
 
     res.status(200).json({
       status: 'success',
+      data: {
+        data: document,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: document,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) {
+      query = query.populate(popOptions);
+    }
+
+    const document = await query;
+
+    if (!document) {
+      return next(new AppError('Document not found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        document,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    //Filtering reviews by tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const document = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: document.length,
       data: {
         data: document,
       },
